fe18a89f4034fd299bb33c8bba339caaab81cd9c
Mehran Naghizadeh <tarhineh@gmail.com>
2023-02-24 20:25:46 +0100

    Add a test

--- lib/mehran.rb
+++ lib/mehran.rb
@@ -1,5 +1,6 @@
-class Mehran
+class Array
   # @return [[int], [int]] The first array contains all even numbers. The second one contains all odd numbers
+  def partition_by_parity
     partition(&:even?)
   end
 end
